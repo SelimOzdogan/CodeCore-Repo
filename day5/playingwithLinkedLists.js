05cd20dba68611b765c76475e489bcdae8ea3b58
Selim <SelimOzdogan@hotmail.com>
2020-03-16 13:19:13 -0700

    playingwithlinkedlist

--- day5/playingwithLinkedLists.js
+++ day5/playingwithLinkedLists.js
@@ -1,23 +1,18 @@
 const listToArr = {
-    list: {},
     createList: function createListformArr(arr) {
-        let value = arr.pop();
-        if (value == undefined)
-            return null;
-        this.list['value'] = value;
-        this.list['next'] = createArrfromList(arr);
-        return this.list;
+        let value = arr.shift();
+        let obj = {};
+        obj['value'] = value;
+        obj['next'] = (arr.length === 0 ? null : createListformArr(arr));
+        return obj;
     },
-    createArr: function createArrfromList(list) {
-         let arr;
-
+    createArr: function createArrfromList(list, arr = []) {
         if (list.next != null) {
-            // console.log(list.next);
-            this.createArr(list.next);
+            this.createArr(list.next, arr);
+            arr.unshift(list.value);
         } else {
             arr.unshift(list.value);
         }
-
         return arr;
     }
 }
@@ -25,3 +20,7 @@ const listToArr = {
 console.log(listToArr.createArr({ value: 1, next: null })) // returns [1]
 
 console.log(listToArr.createArr({ value: 'a', next: { value: 'b', next: { value: 'c', next: null } } })) // returns ['a', 'b', 'c']
+
+console.log(listToArr.createList([4])) // returns {value: 4, next: null}
+
+console.log(listToArr.createList([4, 3, 2, 1])) // {value: 4, next: {value: 3, next: {value: 2, next: {value: 1, next: null}
