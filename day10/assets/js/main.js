f02334957dc1e34416cd60d65e9723733719d3b8
Selim OZDOGAN <selimozdogan@Selims-MacBook-Pro.local>
2020-03-21 20:55:31 -0700

    changed folders name

---
+++ day10/assets/js/main.js
@@ -0,0 +1,10 @@
+const plus = (a, b) => a + b;
+const numbers = [1, 2, 3, 4, 5];
+
+const reduce = (arr, callback, currentvalue) => {
+    if (arr.length === 0) {
+        return callback(0, currentvalue);
+    }
+    let value = arr.pop();
+    return currentvalue + reduce(arr, callback, value);
+};l
