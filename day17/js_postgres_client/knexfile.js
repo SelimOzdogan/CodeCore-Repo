47301a50fe761517fb49f5f6fb54982bd3c0c317
Selim OZDOGAN <selimozdogan@Selims-MacBook-Pro.local>
2020-03-31 10:20:16 -0700

    setup project

--- day17/js_postgres_client/knexfile.js
+++ day17/js_postgres_client/knexfile.js
@@ -3,42 +3,18 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+    client: 'pg',
 
-  staging: {
-    client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'knex_demo'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'migrations',
+directory: './db/migrations'
+    },
+    seeds:{
+      directory: './db/seeds'
     }
   }
-
 };
